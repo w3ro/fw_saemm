a8fc23ff47a63fb4d6fddde4ad63fffc45713ea3
Ricardo Gómez <38111872+w3ro@users.noreply.github.com>
2019-01-07 20:32:15 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,2 @@
+$(document).foundation();
+setTimeout(function(){$('a[target="_blank"]').each(function(){var a=$(this).html();0==a.includes('<i class')&&$(this).append('<i class="material-icons md-1t">exit_to_app</i>')})},1e3),window.setInterval(function(){$('a[target="_blank"]').each(function(){var a=$(this).html();0==a.includes('<i class')&&$(this).append('<i class="material-icons md-1t">exit_to_app</i>')})},1e3);
