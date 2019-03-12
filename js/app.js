cf039e7c3439cf7be3d2cef706615e07c9e9f9e1
Ricardo Gomez <ricardo.gomez@sesaemm.org.mx>
2019-03-12 16:59:28 -0600

    ya

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 $(document).foundation();
 setTimeout(function(){$('a[target="_blank"]').each(function(){var a=$(this).html();0==a.includes('<i class')&&$(this).append('<i class="material-icons md-1t">exit_to_app</i>')})},1e3),window.setInterval(function(){$('a[target="_blank"]').each(function(){var a=$(this).html();0==a.includes('<i class')&&$(this).append('<i class="material-icons md-1t">exit_to_app</i>')})},1e3);
-ready(function(){$('#listado').DataTable();});
+$(document).ready(function(){$('#listado').DataTable();});
