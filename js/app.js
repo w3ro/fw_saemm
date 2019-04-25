a6de84d78189575255d6e84cdc807a9095c5b00f
Ricardo Gomez <ricardo.gomez@sesaemm.org.mx>
2019-04-25 10:17:54 -0500

    b

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 Foundation.Abide.defaults.patterns['curp'] = /^[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$/; //Patron personalizado para CURP
 
-Foundation.Abide.defaults.patterns['rfc_pf'] = /^(([A-Z]{4})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|(([A-Z]{4})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|(([A-Z]{4})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|(([A-Z]{4})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$/; //Patron personalizado para RFC de persona fisica
+Foundation.Abide.defaults.patterns['rfc_pf'] = /^(([A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1]))([A-Z0-9]{3}))|([A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1]))$/; //Patron personalizado para RFC de persona fisica
 
-Foundation.Abide.defaults.patterns['rfc_pm'] = /^(([A-Z]{3})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|(([A-Z]{3})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|(([A-Z]{3})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|(([A-Z]{3})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$/; //Patron personalizado para RFC de persona moral
+Foundation.Abide.defaults.patterns['rfc_pm'] = /^(([A-Z]{3}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1]))([A-Z0-9]{3}))$/; //Patron personalizado para RFC de persona moral
 
 $(document).foundation();
 setTimeout(function(){$('a[target="_blank"]').each(function(){var a=$(this).html();0==a.includes('<i class')&&$(this).append('<i class="material-icons md-1t">exit_to_app</i>')})},1e3),window.setInterval(function(){$('a[target="_blank"]').each(function(){var a=$(this).html();0==a.includes('<i class')&&$(this).append('<i class="material-icons md-1t">exit_to_app</i>')})},1e3);
